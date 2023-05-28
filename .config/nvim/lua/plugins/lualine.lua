2244b2ed5703f3a2febeb10d1482f29f2b36f032
ReneRuprecht <rene.ruprecht.396@gmail.com>
2023-05-28 12:05:43 +0200

    Fix: typo

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-lualine/lualine.nvim',
-    depenencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
+    dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
     config = function()
         require('lualine').setup()
     end
