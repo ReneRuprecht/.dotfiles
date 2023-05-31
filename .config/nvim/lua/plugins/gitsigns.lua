228412da1c5c2222b70c9ddb194bf0302872eb96
ReneRuprecht <rene.ruprecht.396@gmail.com>
2023-05-31 11:17:38 +0200

    Add: markdown-preview, gitsigns & comment

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,7 @@
+return {
+    'lewis6991/gitsigns.nvim',
+    -- version = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+    config = function()
+        require('gitsigns').setup()
+    end,
+}
