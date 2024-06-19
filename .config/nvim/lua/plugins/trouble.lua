c01d3ed0d69c018dc8dc754d4c05699736dbf610
Rene Ruprecht <rene.ruprecht.396@gmail.com>
2024-06-19 23:17:58 +0200

    changed: trouble config

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,13 +1,13 @@
 return {
     "folke/trouble.nvim",
     dependencies = { { 'nvim-tree/nvim-web-devicons' } },
-    config = function()
-        require("trouble").setup {
-            vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end),
-            vim.keymap.set("n", "[d", function() require("trouble").next({ skip_groups = true, jump = true }); end),
-            vim.keymap.set("n", "]d", function()
-                require("trouble").previous({ skip_groups = true, jump = true });
-            end),
-        }
-    end
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+    },
 }
